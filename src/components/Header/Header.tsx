571f8818291a693fff55e6c50670d1858ec9bddf
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2023-03-23 20:49:40 +0200

    added components Header, MenuItem

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<header>
+			<div>left</div>
+			<div>right</div>
+		</header>
+	);
+};
+
+export default Header;
---
+++ src/components/MenuItem/MenuItem.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MenuItem = () => {
+	return (
+		<div>
+			Menu
+		</div>
+	);
+};
+
+export default MenuItem;
