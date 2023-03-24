918ec03dca6237b9ce0f2bdac8f32e5afa54daf5
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2023-03-24 03:21:40 +0300

    adding about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const About = () => {
+	return (
+		<div className="max-w-6xl mx-auto space-y-4">
+			<h1 className="text-2xl font-medium text-amber-600">About</h1>
+			<p className="">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi in molestiae nihil quas ratione rem,
+				voluptate. Alias assumenda deleniti, fugiat illo magni minus natus non, provident, quisquam quod repellat
+				repellendus.
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi in molestiae nihil quas ratione rem,
+				voluptate. Alias assumenda deleniti, fugiat illo magni minus natus non, provident, quisquam quod repellat
+				repellendus.
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi in molestiae nihil quas ratione rem,
+				voluptate. Alias assumenda deleniti, fugiat illo magni minus natus non, provident, quisquam quod repellat
+				repellendus.
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi in molestiae nihil quas ratione rem,
+				voluptate. Alias assumenda deleniti, fugiat illo magni minus natus non, provident, quisquam quod repellat
+				repellendus.
+			</p>
+			<p className="">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi in molestiae nihil quas ratione rem,
+				voluptate. Alias assumenda deleniti, fugiat illo magni minus natus non, provident, quisquam quod repellat
+				repellendus.
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi in molestiae nihil quas ratione rem,
+				voluptate. Alias assumenda deleniti, fugiat illo magni minus natus non, provident, quisquam quod repellat
+				repellendus.
+			</p>
+		</div>
+	);
+};
+
+export default About;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const MyComponent = () => {
 	return (
 		<div>
-
+			MAIN PAGE
 		</div>
 	);
 };
