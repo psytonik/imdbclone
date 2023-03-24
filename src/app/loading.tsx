9bd5f227fb2d100440f51efa6e94c06fab884bf5
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2023-03-24 19:37:18 +0300

    Loading handler

---
+++ src/app/loading.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react';
+
+const Loading = () => {
+	return (
+		<div className="flex justify-center">
+			<img src="/spinner.svg" alt="loading..." className="mt-10 h-50"/>
+		</div>
+	);
+};
+
+export default Loading;
